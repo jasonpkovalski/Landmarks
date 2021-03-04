7661ba73568aa3cf70cb3239e313ba011090a1fc
jasonpkovalski <jasonpkovalski@gmail.com>
2021-03-04 17:56:59 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jason Paul Kovalski on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
